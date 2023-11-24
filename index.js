9b17782180f3cf74a15c08711d7ed71213e9abc4
carssamm <sammy-jo.carstens@hotmail.com>
2023-11-24 08:57:53 +0100

    Added Product dropdown on mobile

--- index.js
+++ index.js
@@ -51,6 +51,8 @@ function openNav() {
     document.getElementById("title").style.marginLeft = "0";
     document.getElementById("offcanvasymbol").style.visibility = "visible";
     document.getElementById("header").style.background = "#d9d9d9";
+    var dropDownElements = document.querySelector('.drpdwn-content');
+    dropDownElements.classList.remove('active-mobile');
     document.body.style.backgroundColor = "#f0f0f0";
     for (var i = 0; i < document.images.length; i++) {
         // Setze den Filter für Helligkeit auf 100%
@@ -60,14 +62,29 @@ function openNav() {
   
   // Shows the dropdown menu by adding the 'active' CSS class
   function showDropDown() {
-      var dropDownElements = document.querySelector('.drpdwn-content');
-      dropDownElements.classList.add('active');
+    var windowWidth = window.innerWidth;
+    if (windowWidth <= 767){
+        var dropDownElements = document.querySelector('.drpdwn-content');
+        dropDownElements.classList.add('active-mobile');
+    }
+    else{
+        var dropDownElements = document.querySelector('.drpdwn-content');
+        dropDownElements.classList.add('active');
+    }
   }
   
   // Hides the dropdown menu by removing the 'active' CSS class
   function hideDropDown() {
-      var dropDownElements = document.querySelector('.drpdwn-content');
-      dropDownElements.classList.remove('active');
+    var windowWidth = window.innerWidth;
+    if (windowWidth <= 767){
+        var dropDownElements = document.querySelector('.drpdwn-content');
+        dropDownElements.classList.remove('active-mobile');
+    }
+    else{
+        var dropDownElements = document.querySelector('.drpdwn-content');
+        dropDownElements.classList.remove('active');
+    }
+
   }
   
   // Adds an event listener to reload the page when the window resolution changes
