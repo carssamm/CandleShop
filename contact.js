fa0367a3c8d7352fad9aa454f094ee3124eef392
wneid <r.eid@web-netz.de>
2024-02-23 08:17:06 +0100

    Hotfix contact.js

--- contact.js
+++ contact.js
@@ -1,11 +1,7 @@
-/**
- * ricardo.eid@bbs1-lueneburg.de
- * 129B6215BEAABE2597C49CBE559D969FA6C2
- * smtp.elasticemail.com
- * 2525
- */
 window.addEventListener('load', function () {
-    console.log("fully loaded")
+
+    const contactMail = "ricardo.eid@bbs1-lueneburg.de"
+
     const form = document.querySelector("#contact-form");
     form.addEventListener("submit", e => {
         e.preventDefault();
@@ -18,18 +14,18 @@ window.addEventListener('load', function () {
         document.querySelector("#contact-form").reset();
 
         console.log("test")
-        sendEmail(name, email, lastname, msg);
+        sendEmail(contactMail, name, email, lastname, msg);
     })
 
-    function sendEmail(name, email, lastname, msg) {
+    function sendEmail(contactMail, name, email, lastname, msg) {
         Email.send({
             Host: "smtp.elasticemail.com",
             Username: "ricardo.eid@bbs1-lueneburg.de",
             Password: "129B6215BEAABE2597C49CBE559D969FA6C2",
-            To: email,
+            To: contactMail,
             From: "no-reply@candleshop.de",
-            Subject: `${name} Just messaged you from the website form`,
-            Body: `${name} <br> ${lastname} <br> Email: ${email} <br> ${msg} <br>`,
+            Subject: `${name} CandleShop Contact Form`,
+            Body: `${name} ${lastname} hat eine Nachricht hinterlassen <br> ${msg} <br>`,
         }).then((success) => {
 
             alert("message sent successfully. Please check the spam folder in your mail.");
